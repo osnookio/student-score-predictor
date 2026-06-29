ede1bef575ebf73ef7ab3ae8feaf1595847a57bd
osnookio <os360nookio@gmail.com>
2026-06-29 23:00:49 +0500

    Remove pickle

--- app.py
+++ app.py
@@ -1,30 +1,13 @@
-from flask import Flask, render_template, request
-import pickle
-
-app = Flask(__name__)
-
+import pandas as pd
+from sklearn.linear_model import LinearRegression
 import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-model_path = os.path.join(BASE_DIR, "model.pkl")
 
-model = pickle.load(open(model_path, "rb"))
+data = pd.read_csv(os.path.join(BASE_DIR, "student_scores.csv"))
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+X = data[["Hours", "Attendance"]]
+y = data["Score"]
 
-@app.route("/predict", methods=["POST"])
-def predict():
-    hours = float(request.form["hours"])
-    attendance = float(request.form["attendance"])
-
-    prediction = model.predict([[hours, attendance]])
-
-    return render_template(
-        "index.html",
-        prediction=round(prediction[0], 2)
-    )
-
-if __name__ == "__main__":
-    app.run(debug=True)
+model = LinearRegression()
+model.fit(X, y)
